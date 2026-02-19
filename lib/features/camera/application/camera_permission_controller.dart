7d9f603ced2c61153acc9900d717d273b37eaac4
Dudley89 <dud1989@gmail.com>
2026-02-19 20:21:33 +0100

    fix: use CardThemeData for ThemeData.cardTheme compatibility

---
+++ lib/features/camera/application/camera_permission_controller.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../../../shared/logging/app_logger.dart';
+
+final cameraPermissionControllerProvider =
+    StateNotifierProvider<CameraPermissionController, PermissionStatus>(
+  (ref) => CameraPermissionController(),
+);
+
+class CameraPermissionController extends StateNotifier<PermissionStatus> {
+  CameraPermissionController() : super(PermissionStatus.denied) {
+    refreshStatus();
+  }
+
+  Future<void> refreshStatus() async {
+    final status = await Permission.camera.status;
+    state = status;
+    AppLogger.info('Stato permesso camera: $status');
+  }
+
+  Future<void> requestPermission() async {
+    final status = await Permission.camera.request();
+    state = status;
+    if (status.isGranted) {
+      AppLogger.info('Permesso camera concesso');
+    } else {
+      AppLogger.warn('Permesso camera non concesso: $status');
+    }
+  }
+}
